bd634b2f94da42fe1baeb6a487b7ca9fe702f26c
astonishing-installation-0408 <dlbuesk@gmail.com>
2019-07-01 17:50:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,10 +11,11 @@ function logWhisper (string) {
   console.log (string.toLowerCase ()) ;
 }
 
-function sayHiToGrandma (string) { 
-  if (string.toLowerCase () === string) { return "I can't hear you!"
+function sayHiToGrandma (string) {
+ if (string.toLowerCase () === string) { return "I can't hear you!" ;
 }
 
 if (string.toUpperCase () === string) {
-return "YES INDEED!" 
+return "YES INDEED!" ;
 }
+if string
